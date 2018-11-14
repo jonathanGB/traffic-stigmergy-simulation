3ff34dbdf4543e53681b871db69faf893d1cd965
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2018-11-14 16:53:57 -0500

    Added getters/setters to the Cache

--- cache.py
+++ cache.py
@@ -9,3 +9,21 @@ class Cache:
     self.short = None
     self.long = None
     self.anticip = None
+
+  def get_short(self):
+    return self.short
+
+  def get_long(self):
+    return self.long
+
+  def get_anticip(self):
+    return self.anticip
+
+  def set_short(self, short):
+    self.short = short
+
+  def set_long(self, long):
+    self.long = long
+
+  def set_anticip(self, anticip):
+    self.anticip = anticip
